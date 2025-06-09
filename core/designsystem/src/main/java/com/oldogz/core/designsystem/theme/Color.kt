8959768d9ba815d18e35b3bd0dc39f3667db880e
p-chanmin <pc_m111@naver.com>
2025-06-09 19:18:24 +0900

    feat: compose theme 설정

--- core/designsystem/src/main/java/com/oldogz/core/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/oldogz/core/designsystem/theme/Color.kt
@@ -2,10 +2,16 @@ package com.oldogz.core.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val EerieBlack = Color(0xFF0D1A12) // 글씨색
+val White = Color(0xFFFFFFFF) // 글씨색
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Malachite = Color(0xFF38E078) // 주요 색
+
+val SeaGreen = Color(0xFF52946B) // 서브 글씨
+val CambridgeBlue = Color(0xFF96C4A8)
+
+val MintCream = Color(0xFFE8F2ED) // 서브 배경
+val BrunswickGreen = Color(0xFF264533)
+
+val SeaSalt = Color(0xFFF7FAFA) // 배경
+val DarkGreen = Color(0xFF122117)
