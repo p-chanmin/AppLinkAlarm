e4cbd2b71b5ea41e8fb86a0d31d375c1fac0ef11
p-chanmin <pc_m111@naver.com>
2025-06-15 20:54:18 +0900

    test: Repository, DataSource Test 추가

---
+++ core/database/src/test/java/com/oldogz/core/database/SettingDataSourceTest.kt
@@ -0,0 +1,100 @@
+package com.oldogz.core.database
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import app.cash.turbine.test
+import com.oldogz.core.database.datasource.SettingDataSource
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.rules.TemporaryFolder
+
+internal class SettingDataSourceTest : StringSpec() {
+
+    private lateinit var testDispatcher: TestDispatcher
+    private lateinit var tempFolder: TemporaryFolder
+    private lateinit var dataStore: DataStore<Preferences>
+    private lateinit var settingDataSource: SettingDataSource
+
+    init {
+
+        beforeSpec {
+            testDispatcher = StandardTestDispatcher()
+            tempFolder = TemporaryFolder.builder().assureDeletion().build().apply { create() }
+
+            dataStore = PreferenceDataStoreFactory.create(
+                scope = CoroutineScope(testDispatcher),
+                produceFile = { tempFolder.newFile("test.preferences_pb") }
+            )
+            settingDataSource = SettingDataSource(dataStore)
+        }
+
+        afterSpec {
+            tempFolder.delete()
+        }
+
+        "lastReviewDate 초기 상태 테스트" {
+            runTest(testDispatcher) {
+                // Given
+
+                // When
+                settingDataSource.lastReviewDate.test {
+
+                    // Then
+                    awaitItem() shouldBe null
+                    cancelAndConsumeRemainingEvents()
+                }
+            }
+        }
+
+        "rejectFlexibleUpdateDate 초기 상태 테스트" {
+            runTest(testDispatcher) {
+                // Given
+
+                // When
+                settingDataSource.rejectFlexibleUpdateDate.test {
+
+                    // Then
+                    awaitItem() shouldBe null
+                    cancelAndConsumeRemainingEvents()
+                }
+            }
+        }
+
+        "lastReviewDate 저장 및 조회 테스트" {
+            runTest(testDispatcher) {
+                // Given
+                val result = "2025-05-02T11:09:35.545010Z"
+                settingDataSource.setLastReviewDate(result)
+
+                // When
+                settingDataSource.lastReviewDate.test {
+
+                    // Then
+                    awaitItem() shouldBe result
+                    cancelAndConsumeRemainingEvents()
+                }
+            }
+        }
+
+        "rejectFlexibleUpdateDate 저장 및 조회 테스트" {
+            runTest(testDispatcher) {
+                // Given
+                val result = "2025-05-02T11:09:35.545010Z"
+                settingDataSource.setRejectFlexibleUpdateDate(result)
+
+                // When
+                settingDataSource.rejectFlexibleUpdateDate.test {
+
+                    // Then
+                    awaitItem() shouldBe result
+                    cancelAndConsumeRemainingEvents()
+                }
+            }
+        }
+    }
+}
