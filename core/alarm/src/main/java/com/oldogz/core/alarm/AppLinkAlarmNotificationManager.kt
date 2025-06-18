a737ce13b17552e80adf181b17d30a6b2c10e414
p-chanmin <pc_m111@naver.com>
2025-06-18 20:39:37 +0900

    feat: 기본 AlarmManager logic 추가

--- core/alarm/src/main/java/com/oldogz/core/alarm/AppLinkAlarmNotificationManager.kt
+++ core/alarm/src/main/java/com/oldogz/core/alarm/AppLinkAlarmNotificationManager.kt
@@ -1,6 +1,19 @@
 package com.oldogz.core.alarm
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import androidx.annotation.StringRes
+import androidx.core.graphics.createBitmap
+import com.oldogz.core.model.AlarmMode
+import com.oldogz.core.model.AppLinkAlarm
+import com.oldogz.core.navigation.getDeepLinkOf
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -9,4 +22,135 @@ import javax.inject.Singleton
 class AppLinkAlarmNotificationManager @Inject constructor(
     @ApplicationContext private val context: Context
 ) {
+
+    private val notificationManager: NotificationManager =
+        context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    init {
+        registerNotificationChannels()
+    }
+
+    fun registerNotificationChannels() {
+        println("registerNotificationChannels")
+        createNotificationChannel(
+            R.string.core_alarm_notification_channel_name,
+            R.string.core_alarm_notification_channel_description,
+            CHANNEL_ID_APP_LINK_ALARM,
+            NotificationManager.IMPORTANCE_HIGH
+        )
+    }
+
+    fun notify(id: Int, notification: Notification) {
+        notificationManager.notify(id, notification)
+    }
+
+    fun cancel(id: Int) {
+        notificationManager.cancel(id)
+    }
+
+    fun createNotification(appLinkAlarm: AppLinkAlarm, includeAds: Boolean): Notification {
+        return when (appLinkAlarm.alarmMode) {
+            AlarmMode.ONLY_NOTIFICATION -> createNotificationOnly(appLinkAlarm, includeAds)
+            AlarmMode.STANDARD -> createNotificationStandard(appLinkAlarm, includeAds)
+        }
+    }
+
+    private fun createNotificationOnly(
+        appLinkAlarm: AppLinkAlarm,
+        includeAds: Boolean
+    ): Notification {
+        return Notification.Builder(context, CHANNEL_ID_APP_LINK_ALARM)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setLargeIcon(createAppIconBitmap(appLinkAlarm.linkedAppPackage))
+            .setContentTitle(appLinkAlarm.alarmName)
+            .setContentText(appLinkAlarm.alarmMessage)
+            .setStyle(Notification.BigTextStyle().bigText(appLinkAlarm.alarmMessage))
+            .setContentIntent(createPendingIntent(appLinkAlarm, includeAds))
+            .setDeleteIntent(createPendingIntent(appLinkAlarm, includeAds))
+            .setAutoCancel(true)
+            .build()
+    }
+
+    private fun createNotificationStandard(
+        appLinkAlarm: AppLinkAlarm,
+        includeAds: Boolean
+    ): Notification {
+        return Notification.Builder(context, CHANNEL_ID_APP_LINK_ALARM)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setLargeIcon(createAppIconBitmap(appLinkAlarm.linkedAppPackage))
+            .setContentTitle(appLinkAlarm.alarmName)
+            .setContentText(appLinkAlarm.alarmMessage)
+            .setStyle(Notification.BigTextStyle().bigText(appLinkAlarm.alarmMessage))
+            .setContentIntent(createPendingIntent(appLinkAlarm, includeAds))
+            .setAutoCancel(true)
+            .build()
+    }
+
+    private fun createAppIconBitmap(linkedAppPackage: String): Bitmap {
+        val packageManager = context.packageManager
+        val appInfo = packageManager.getApplicationInfo(linkedAppPackage, 0)
+        val drawable = packageManager.getApplicationIcon(appInfo)
+
+        val width = drawable.intrinsicWidth.takeIf { it > 0 } ?: 1
+        val height = drawable.intrinsicHeight.takeIf { it > 0 } ?: 1
+        val bitmap = createBitmap(width, height)
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, canvas.width, canvas.height)
+        drawable.draw(canvas)
+        return bitmap
+    }
+
+    private fun createShowIntent(appLinkAlarm: AppLinkAlarm, includeAds: Boolean): Intent? {
+        return when (includeAds) {
+            true -> Intent(Intent.ACTION_VIEW, getDeepLinkOf("open/${appLinkAlarm.id}"))
+            false -> context.packageManager.getLaunchIntentForPackage(appLinkAlarm.linkedAppPackage)
+        }
+    }
+
+    private fun createPendingIntent(
+        appLinkAlarm: AppLinkAlarm,
+        includeAds: Boolean
+    ): PendingIntent {
+
+        val intent = createShowIntent(appLinkAlarm, includeAds)
+
+        return when (includeAds) {
+            true -> {
+                TaskStackBuilder.create(context).run {
+                    addNextIntentWithParentStack(intent)
+                    getPendingIntent(
+                        appLinkAlarm.id,
+                        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                    )
+                }
+            }
+
+            false -> {
+                PendingIntent.getActivity(
+                    context,
+                    appLinkAlarm.id,
+                    intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                )
+            }
+        }
+    }
+
+    private fun createNotificationChannel(
+        @StringRes notificationNameResId: Int,
+        @StringRes notificationDescriptionResId: Int,
+        channelId: String,
+        importance: Int
+    ) {
+        val name = context.getString(notificationNameResId)
+        val descriptionText = context.getString(notificationDescriptionResId)
+        val channel = NotificationChannel(channelId, name, importance).apply {
+            description = descriptionText
+        }
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    companion object {
+        const val CHANNEL_ID_APP_LINK_ALARM = "channelIdAppLinkAlarm"
+    }
 }
--- core/alarm/src/main/java/com/oldogz/core/alarm/AppLinkAlarmReceiver.kt
+++ core/alarm/src/main/java/com/oldogz/core/alarm/AppLinkAlarmReceiver.kt
@@ -3,15 +3,79 @@ package com.oldogz.core.alarm
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.oldogz.core.alarm.AppLinkAlarmManager.Companion.INTENT_ACTION_APP_LINK_ALARM
+import com.oldogz.core.alarm.AppLinkAlarmManager.Companion.INTENT_EXTRA_APP_LINK_ALARM_ID
+import com.oldogz.core.data.AppLinkAlarmRepository
+import com.oldogz.core.model.AlarmMode
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class AppLinkAlarmReceiver : BroadcastReceiver() {
 
+    @Inject
+    lateinit var appLinkAlarmRepository: AppLinkAlarmRepository
+
+    @Inject
+    lateinit var appLinkAlarmManager: AppLinkAlarmManager
+
+    @Inject
+    lateinit var appLinkAlarmNotificationManager: AppLinkAlarmNotificationManager
 
     override fun onReceive(context: Context, intent: Intent) {
+
         when (intent.action) {
-            Intent.ACTION_BOOT_COMPLETED -> {}
+            Intent.ACTION_BOOT_COMPLETED, Intent.ACTION_TIMEZONE_CHANGED -> {
+                println("onReceiveACTION_BOOT_COMPLETED, ACTION_TIMEZONE_CHANGED(${intent.action})")
+                CoroutineScope(Dispatchers.IO).launch {
+                    val appLinkAlarms = appLinkAlarmRepository.alarms.first()
+                    appLinkAlarms.forEach { appLinkAlarm ->
+                        appLinkAlarmManager.scheduleAlarm(appLinkAlarm)
+                    }
+                }
+            }
+
+            INTENT_ACTION_APP_LINK_ALARM -> {
+                appLinkAlarmNotificationManager.registerNotificationChannels()
+                val id = intent.getIntExtra(INTENT_EXTRA_APP_LINK_ALARM_ID, -1)
+                if (id == -1) return
+                // 알림 출력 or 서비스 실행
+                println("onReceive INTENT_ACTION_APP_LINK_ALARM($id)")
+
+                CoroutineScope(Dispatchers.IO).launch {
+                    val appLinkAlarm = appLinkAlarmRepository.getAlarmById(id).first()
+
+                    when (appLinkAlarm.alarmMode) {
+                        AlarmMode.ONLY_NOTIFICATION -> {
+                            println("AlarmMode.ONLY_NOTIFICATION")
+                            appLinkAlarmNotificationManager.notify(
+                                appLinkAlarm.id,
+                                appLinkAlarmNotificationManager.createNotification(
+                                    appLinkAlarm,
+                                    true
+                                )
+                            )
+                        }
+
+                        AlarmMode.STANDARD -> {
+                            println("AlarmMode.STANDARD")
+                            appLinkAlarmNotificationManager.notify(
+                                appLinkAlarm.id,
+                                appLinkAlarmNotificationManager.createNotification(
+                                    appLinkAlarm,
+                                    true
+                                )
+                            )
+                        }
+                    }
+
+                    appLinkAlarmManager.scheduleAlarm(appLinkAlarm)
+                }
+            }
         }
     }
 }
