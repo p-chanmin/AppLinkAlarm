07df27f2ccfd579724780948ce0bcc1346be23ae
p-chanmin <pc_m111@naver.com>
2025-06-30 05:19:16 +0900

    feat: FirebaseAnalytics Service Event 연결

--- core/alarm/src/main/java/com/oldogz/core/alarm/AppLinkAlarmReceiver.kt
+++ core/alarm/src/main/java/com/oldogz/core/alarm/AppLinkAlarmReceiver.kt
@@ -3,11 +3,14 @@ package com.oldogz.core.alarm
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.google.firebase.analytics.logEvent
 import com.oldogz.core.alarm.AppLinkAlarmManager.Companion.INTENT_ACTION_APP_LINK_ALARM
 import com.oldogz.core.alarm.AppLinkAlarmManager.Companion.INTENT_EXTRA_APP_LINK_ALARM_ID
 import com.oldogz.core.alarm.AppLinkAlarmPlayingService.Companion.INTENT_ACTION_SERVICE_APP_LINK_ALARM_ON
 import com.oldogz.core.alarm.AppLinkAlarmPlayingService.Companion.INTENT_EXTRA_SERVICE_APP_LINK_ALARM_ID
 import com.oldogz.core.data.AppLinkAlarmRepository
+import com.oldogz.core.firebase.FirebaseManager
+import com.oldogz.core.firebase.model.FA
 import com.oldogz.core.model.AlarmMode
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
@@ -27,6 +30,9 @@ class AppLinkAlarmReceiver : BroadcastReceiver() {
 
     @Inject
     lateinit var appLinkAlarmNotificationManager: AppLinkAlarmNotificationManager
+
+    @Inject
+    lateinit var firebaseManager: FirebaseManager
 
     private val includeAds = true // 광고
 
@@ -53,8 +59,12 @@ class AppLinkAlarmReceiver : BroadcastReceiver() {
                     val appLinkAlarm = appLinkAlarmRepository.getAlarmById(id).first()
 
                     if (isAppInstalled(context, appLinkAlarm.linkedAppPackage)) {
+                        firebaseManager.firebaseAnalytics.logEvent(FA.Event.ALARM_TRIGGERED) {
+                            param(FA.Param.Key.ALARM_MODE, appLinkAlarm.alarmMode.name)
+                        }
                         when (appLinkAlarm.alarmMode) {
                             AlarmMode.NOTIFICATION_ONLY -> {
+
                                 appLinkAlarmNotificationManager.notify(
                                     appLinkAlarm.id,
                                     appLinkAlarmNotificationManager.createNotification(
@@ -78,6 +88,7 @@ class AppLinkAlarmReceiver : BroadcastReceiver() {
                         }
                         appLinkAlarmManager.scheduleAlarm(appLinkAlarm)
                     } else {
+                        firebaseManager.firebaseAnalytics.logEvent(FA.Event.LINKED_APP_NOT_FOUND) {}
                         appLinkAlarmRepository.updateAlarm(appLinkAlarm.copy(active = false))
                         appLinkAlarmNotificationManager.notify(
                             appLinkAlarm.id,
