94b7cd299db65ddc038710ab456f455752873084
p-chanmin <pc_m111@naver.com>
2025-07-02 02:27:32 +0900

    feat: In-app-update 연결

--- feature/main/build.gradle.kts
+++ feature/main/build.gradle.kts
@@ -6,7 +6,7 @@ android {
     namespace = "com.oldogz.applinkalarm.feature.main"
 }
 dependencies {
+    implementation(libs.play.app.update.ktx)
     implementation(project(":feature:alarm"))
     implementation(project(":feature:setting"))
 }
-
