bf9c8c7323b873f76f5c7a810d237a7e0e9ca8d9
p-chanmin <pc_m111@naver.com>
2025-07-02 20:59:51 +0900

    feat: Version code 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.oldogz.applinkalarm"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
