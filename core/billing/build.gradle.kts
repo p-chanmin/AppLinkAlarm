5f7c21a645c54c7867fdaefeea13fda0c321db00
p-chanmin <pc_m111@naver.com>
2025-07-06 04:11:23 +0900

    feat: billing libs 변경 및 local properties 설정

--- core/billing/build.gradle.kts
+++ core/billing/build.gradle.kts
@@ -1,11 +1,23 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
     id("oldogz.android.library")
+    id("oldogz.android.compose")
 }
 
 android {
     namespace = "com.oldogz.core.billing"
+
+    defaultConfig {
+        buildConfigField("String", "PREMIUM_MEMBERSHIP_PRODUCT_ID", "\"${getPropertyKey("PREMIUM_MEMBERSHIP_PRODUCT_ID")}\"")
+        buildConfigField("String", "PREMIUM_MONTHLY_TRIAL_7D_OFFER_ID", "\"${getPropertyKey("PREMIUM_MONTHLY_TRIAL_7D_OFFER_ID")}\"")
+    }
+}
+
+fun getPropertyKey(propertyKey: String): String {
+    return gradleLocalProperties(rootDir, providers).getProperty(propertyKey)
 }
 
 dependencies {
-    implementation(libs.billing)
+    api(libs.billing.ktx)
 }
