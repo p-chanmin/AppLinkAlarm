6ad49035d35bfd7dd8f3edabf60ef73e85a68c58
p-chanmin <pc_m111@naver.com>
2025-07-08 03:51:21 +0900

    v0.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.oldogz.applinkalarm"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.0.4"
+        versionCode = 5
+        versionName = "0.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
